f6a84748f6b800e32e715b8c20ef33e9faf680c7
kite1412 <frozradit@gmail.com>
2025-04-12 15:55:30 +0700

    fix(assignment): remove duplicate authenticate middleware from create assignment api

--- src/routes/assignment.route.js
+++ src/routes/assignment.route.js
@@ -1,16 +1,15 @@
 import express from "express";
-import { 
-  createAssignment, 
-  getAssigmentsByCourseId 
+import {
+  createAssignment,
+  getAssigmentsByCourseId
 } from "../controllers/assignment.controller.js";
-import { authenticate, authorizeRoles } from "../middlewares/auth.js";
+import { authorizeRoles } from "../middlewares/auth.js";
 
 const assignmentRoute = express.Router();
 
 assignmentRoute.get("/course/:courseId", getAssigmentsByCourseId);
 assignmentRoute.post(
   "/:courseId",
-  authenticate,
   authorizeRoles("teacher", "admin"),
   createAssignment
 );
